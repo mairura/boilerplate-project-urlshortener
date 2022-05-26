7850781af7c5a59396465a735a0f64ad826aabe1
unknown <mairurated@gmail.com>
2022-05-26 16:47:37 +0300

    file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +8,55 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+// Urlparser
+app.post("/api/shorturl", function (req, res) {
+  console.log(req.body);
+  const bodyurl = req.body.url;
+
+  const something = dns.lookup(
+    urlparser.parse(bodyurl).hostname,
+    (error, address) => {
+      if (!address) {
+        res.json({ error: "Invalid URL" });
+      } else {
+        const url = new Url({ url: bodyurl });
+        url.save((error, data) => {
+          res.json({
+            original_url: data.url,
+            short_url: data.id,
+          });
+        });
+      }
+      console.log("dns", error);
+      console.log("address", address);
+    }
+  );
+
+  console.log("something", something);
+});
+
+app.get("/api/shorturl/:id", (req, res) => {
+  const id = req.params.id;
+  Url.findById(id, (error, data) => {
+    if (!data) {
+      res.json({ error: "Invalid URL" });
+    } else {
+      res.redirect(data.url);
+    }
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
